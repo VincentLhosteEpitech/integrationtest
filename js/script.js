eb889a3f198d56905898388c66862a3e8f8ac399
vincent <vincent.lhoste@yahoo.fr>
2020-10-23 15:22:43 +0200

    commit général 2

---
+++ js/script.js
@@ -0,0 +1,13 @@
+  
+window.onload = function(){
+  var slider = document.getElementById('slider');
+
+  noUiSlider.create(slider, {
+      start: [20, 80],
+      connect: true,
+      range: {
+          'min': 0,
+          'max': 100
+      }
+  });
+};
